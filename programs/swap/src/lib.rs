00052a328ca9fac8b884d2105686b7cd2f9ce862
Oleksandr_Husiev <ohusiev@icloud.com>
2024-12-03 22:51:16 +0100

    Added initial commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5TqbBrsiZsgAJcaWNsocGBfxtGBE6TXu36EFt1RTp62r");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
