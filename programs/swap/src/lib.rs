da9024235b1354129a4d2bc5ac38801095120dcb
Oleksandr_Husiev <ohusiev@icloud.com>
2024-12-08 20:51:42 +0100

    Added make offer logic to lib.rs

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -15,7 +15,13 @@ declare_id!("5TqbBrsiZsgAJcaWNsocGBfxtGBE6TXu36EFt1RTp62r");
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
     }
 }
